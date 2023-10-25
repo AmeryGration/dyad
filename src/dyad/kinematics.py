fbb1448b4915906f85859a74a0651750160b97fa
Amery Gration <amerygration@proton.me>
2023-10-25 19:43:33 +0100

    Treatment of scalar and ndarray function args sorted out

--- src/dyad/kinematics.py
+++ src/dyad/kinematics.py
@@ -7,7 +7,7 @@ __all__ = [
     "eccentric_anomaly_from_true_anomaly",
     "eccentric_anomaly_from_mean_anomaly",
 ]
-    
+
 import numpy as np
 import scipy as sp
 
@@ -19,7 +19,7 @@ def _check_eccentricity(e):
     if not isinstance(e, (int, float)):
         raise TypeError("e must be scalar.")
 
-    if e < 0. or e >= 1.: 
+    if e < 0. or e >= 1.:
         raise ValueError("e must be nonnegative and less than one.")
 
     return e
@@ -28,19 +28,11 @@ def true_anomaly_from_mean_anomaly(mu, e):
     """Return the true anomaly modulo :math:`2\pi`."""
     e = _check_eccentricity(e)
 
-    # Coerce array `m` here. Later squeeze it to get float as required. See
-    # https://stackoverflow.com/questions/29318459/python-function-that-
-    # handles-scalar-or-arrays
-    mu = np.asarray(mu)
-    if np.isscalar(mu):
-        is_scalar = True
-    else:
-        is_scalar = False
-    
+    if not np.isscalar(mu):
+        mu = np.asarray(mu)
+
     if e == 0.:
         theta = mu%(2.*np.pi)
-        if is_scalar:
-            return theta.squeeze()
 
         return theta
 
@@ -48,58 +40,39 @@ def true_anomaly_from_mean_anomaly(mu, e):
     theta = true_anomaly_from_eccentric_anomaly(eta, e)
     theta = theta%(2.*np.pi)
 
-    if is_scalar:
-        return theta.squeeze()
-    
     return theta
 
 def true_anomaly_from_eccentric_anomaly(eta, e):
     """Return the true anomaly modulo :math:`2\pi`."""
     e = _check_eccentricity(e)
 
-    eta = np.asarray(eta)
-    if np.isscalar(eta):
-        is_scalar = True
-    else:
-        is_scalar = False
-    
+    if not np.isscalar(eta):
+        eta = np.asarray(eta)
+
     if e == 0.:
         theta = eta%(2.*np.pi)
-        if is_scalar:
-            return theta.squeeze()
 
         return theta
 
     theta = 2.*np.arctan(np.sqrt(1. + e)*np.tan(eta/2.)/np.sqrt(1. - e))
     theta = (theta + 2.*np.pi)%(2.*np.pi)
 
-    if is_scalar:
-        return theta.squeeze()
-    
     return theta
 
 def mean_anomaly_from_eccentric_anomaly(eta, e):
     """Return the mean anomaly modulo :math:`2\pi`."""
     e = _check_eccentricity(e)
 
-    eta = np.asarray(eta)
-    if np.isscalar(eta):
-        is_scalar = True
-    else:
-        is_scalar = False
-    
+    if not np.isscalar(eta):
+        eta = np.asarray(eta)
+
     if e == 0.:
         mu = eta%(2.*np.pi)
-        if is_scalar:
-            return theta.squeeze()
 
         return mu
 
     mu = eta - e*np.sin(eta)
     mu = mu%(2.*np.pi)
-
-    if is_scalar:
-        return mu.squeeze()
 
     return mu
 
@@ -107,24 +80,16 @@ def mean_anomaly_from_true_anomaly(theta, e):
     """Return the mean anomaly modulo :math:`2\pi`."""
     e = _check_eccentricity(e)
 
-    theta = np.asarray(theta)
-    if np.isscalar(theta):
-        is_scalar = True
-    else:
-        is_scalar = False
+    if not np.isscalar(theta):
+        theta = np.asarray(theta)
 
     if e == 0.:
         mu = theta%(2.*np.pi)
-        if is_scalar:
-            return mu.squeeze()
-        
+
         return mu
 
     eta = eccentric_anomaly_from_true_anomaly(theta, e)
     mu = mean_anomaly_from_eccentric_anomaly(eta, e)
-
-    if is_scalar:
-        return mu.squeeze()
 
     return mu
 
@@ -132,23 +97,16 @@ def eccentric_anomaly_from_true_anomaly(theta, e):
     """Return the eccentric anomaly modulo :math:`2\pi`."""
     e = _check_eccentricity(e)
 
-    theta = np.asarray(theta)
-    if np.isscalar(theta):
-        is_scalar = True
-    else:
-        is_scalar = False
+    if not np.isscalar(theta):
+        theta = np.asarray(theta)
 
     if e == 0.:
         eta = theta%(2.*np.pi)
-        if is_scalar:
-            return eta.squeeze()
-        
+
         return eta
 
     eta = 2.*np.arctan(np.sqrt(1. - e)*np.tan(theta/2.)/np.sqrt(1. + e))
     eta = (eta + 2.*np.pi)%(2.*np.pi)
-    if is_scalar:
-        return eta.squeeze()
 
     return eta
 
@@ -160,7 +118,7 @@ def eccentric_anomaly_from_mean_anomaly(mu, e):
         return 1. - e*np.cos(eta)
 
     def solve(x):
-        # Keyword factor=1. required to avoid numerical instability for large e
+        # Keyword factor=1. required to avoid numerical instability for big e
         x = x%(2.*np.pi)
 
         res = sp.optimize.fsolve(f, x, x, fprime, factor=1.)
@@ -173,62 +131,75 @@ def eccentric_anomaly_from_mean_anomaly(mu, e):
         res = 1.*np.asarray(res.item())
 
         return res
-        
+
     e = _check_eccentricity(e)
 
-    mu = np.asarray(mu)
-    if np.isscalar(mu):
-        is_scalar = True
-    else:
-        is_scalar = False
+    if not np.isscalar(mu):
+        mu = np.asarray(mu)
 
     if e == 0.:
         eta = mu%(2.*np.pi)
-        if is_scalar:
-            return eta.squeeze()
 
         return eta
 
     eta = np.vectorize(solve)(mu)
     # eta = solve(mu)
-    
-    if is_scalar:
-        return eta.squeeze()
 
     return eta
 
 
 class Orbit:
     def __init__(self, mass, elements):
-        mass = np.asarray(mass)
+        """A class for specifying a body's orbit
+
+        Parameters
+        ----------
+        mass: (n,) array-like
+
+            The effective mass of the attracting body
+
+        elements: (6, n) array-like
+
+            The orbital elements
+
+        """
+        ################################################################
+        # Check shapes of args are consistent
+        ################################################################
         elements = np.asarray(elements)
 
-        if not (mass.ndim == 0 or mass.ndim == 1):
-            raise ValueError("mass must be a scalar or 1d array.")
-        if not (len(elements) == 6):
-            raise ValueError("elements must contain 6 items.")
-        if not (elements.ndim == 1 or elements.ndim == 2):
-            raise ValueError("elements must be a 1d or 2d array.")
-        try:
-            # If m is not a 0d array
-            if not (mass.shape[0] == elements.shape[0]):
-                raise ValueError("mass and elements must have same length.")
-        except IndexError:
-            # If m is a 0d array 
+        if np.isscalar(mass):
             if not elements.ndim == 1:
-                raise ValueError("mass and elements must have same length.")
-        if np.any(mass <= 0.):
+                raise ValueError(
+                    "mass must be (n,) array_like and elements must be "
+                    "(6, n) array_like."
+                )
+        else:
+            mass = np.asarray(mass)
+            if not elements.shape == (6,) + mass.shape:
+                raise ValueError(
+                    "mass must be (n,) array_like and elements must be "
+                    "(6, n) array_like."
+                )
+
+        ################################################################
+        # Check values of args
+        ################################################################
+        if np.any(mass <= 0):
             raise ValueError("mass must be positive.")
         if np.any(elements[0] <= 0.):
             raise ValueError(
                 "first item in elements (semimajor axis) must be positive."
             )
-        if np.any(elements[1] < 0.) or np.any(elements[1] >= 1.): 
+        if np.any(elements[1] < 0.) or np.any(elements[1] >= 1.):
             raise ValueError(
                 "second item in elements (eccentricity) must be nonnegative "
                 "and less than one."
             )
 
+        ################################################################
+        # Attributes
+        ################################################################
         self.mass = mass
         self.semimajor_axis = elements[0]
         self.eccentricity = elements[1]
@@ -284,7 +255,7 @@ class Orbit:
             *self.semimajor_axis
             *(1. - self.eccentricity**2.)
         )
-    
+
     @property
     def angular_momentum(self):
         h = self.angular_momentum_magnitude
@@ -335,7 +306,6 @@ class Orbit:
 
     @property
     def state(self):
-        # tuple of floats
         return np.hstack([self._position(), self._velocity()])
 
     @property
@@ -346,7 +316,6 @@ class Orbit:
         )
 
     def _position(self):
-        # tuple of floats
         r = self.radius()
         x = r*(
             np.cos(self.longitude_of_ascending_node)
@@ -387,7 +356,6 @@ class Orbit:
         )
 
     def _velocity(self):
-        # tuple of floats
         v_magnitude = self.speed()
         v_x = -v_magnitude*(
             np.cos(self.longitude_of_ascending_node)
