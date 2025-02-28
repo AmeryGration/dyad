c2f5da14b8ebd24d8f3e6304cb060158ca6cc134
AmeryGration <amerygration@proton.me>
2025-02-28 16:08:10 +0000

    Investigating secondary mass distribution

--- dyad/stats/data/primary_mass/kroupa2002/plot.py
+++ dyad/stats/data/primary_mass/kroupa2002/plot.py
@@ -6,31 +6,49 @@ mpl.style.use("sm")
 import numpy as np
 from dyad.stats import mass
 
-f = 2.*(
+f_1 = 2.*(
     mass.kroupa2002.pdf(primary_mass_sample)
     *mass.kroupa2002.cdf(primary_mass_sample)
 )
-F = cumulative_trapezoid(f, primary_mass_sample, initial=0.)
+F_1 = cumulative_trapezoid(f_1, primary_mass_sample, initial=0.)
+# f_2 = 2*(
+#     mass.kroupa2002.pdf(primary_mass_sample)
+#     *(1. - mass.kroupa2002.cdf(primary_mass_sample))
+# )
+# F_2 = cumulative_trapezoid(f_2, primary_mass_sample, initial=0.)
 
 a = 0.08
 b = 60.
 primary_mass_boundary = (a, 0.5, 1.2, 3.5, 6., b)
 
-imf = pdf_mass(primary_mass_sample)
+f_M = pdf_mass(primary_mass_sample)
+F_M = mass.kroupa2002().cdf(primary_mass_sample)
 
 fig, ax = plt.subplots()
-ax.plot(primary_mass_sample, imf, ls="dashed")
-ax.plot(primary_mass_sample, f, color="red", ls="solid")
-# ax.plot(primary_mass_sample, F, color="red")
+ax.plot(primary_mass_sample, f_M, ls="dashed")
+ax.plot(primary_mass_sample, f_1, color="red", ls="solid")
 ax.plot(primary_mass_sample, frequency_sample, ls="solid")
-# ax.plot(primary_mass_sample, cumulative_frequency_sample, ls="solid")
 ax.vlines(primary_mass_boundary, 1.e-5, 1.e1, ls="dotted")
 ax.set_xlim(a, b)
 ax.set_ylim(1.e-5, 1.e1)
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_xlabel(r"$m/\text{M}_{\odot}$")
-ax.set_ylabel(r"$f_{M_{1}}$")
+# ax.set_ylabel(r"$f_{M_{1}}$")
+fig.savefig("./Figures/f_M_moe2017_pairing.jpg")
+fig.savefig("./Figures/f_M_moe2017_pairing.pdf")
+plt.show()
+
+fig, ax = plt.subplots()
+ax.plot(primary_mass_sample, F_1, ls="solid")
+ax.plot(primary_mass_sample, F_M, ls="dashed")
+ax.plot(primary_mass_sample, cumulative_frequency_sample, ls="solid")
+ax.vlines(primary_mass_boundary, 0., 1.2, ls="dotted")
+ax.set_xlim(a, b)
+ax.set_ylim(0., 1.2)
+ax.set_xscale("log")
+ax.set_xlabel(r"$m/\text{M}_{\odot}$")
+# ax.set_ylabel(r"$F_{M_{1}}$")
 fig.savefig("./Figures/f_M_moe2017_pairing.jpg")
 fig.savefig("./Figures/f_M_moe2017_pairing.pdf")
 plt.show()
--- dyad/stats/data/secondary_mass/plot.py
+++ dyad/stats/data/secondary_mass/plot.py
@@ -5,72 +5,148 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 from scipy.interpolate import RegularGridInterpolator
+from scipy.integrate import quad
 
 mpl.style.use("sm")
-
-m_min = 0.08
-m_max = 60.
-
-mass_ratio_sample = np.loadtxt("mass_ratio_sample.dat")
-primary_mass_sample = np.loadtxt("primary_mass_sample.dat")
-pdf_sample = np.loadtxt("frequency_sample.dat")
-cdf_sample = np.loadtxt("cumulative_frequency_sample.dat")
+cmap = "viridis"
 
 m_min = 0.08
 m_max = 60.
 primary_mass_boundary = (m_min, 1.2, 3.5, 6., m_max)
 
+mass_ratio_sample = np.loadtxt("mass_ratio_sample.dat")
+primary_mass_sample = np.loadtxt("primary_mass_sample.dat")
+pdf_sample = np.loadtxt("frequency_sample.dat").T
+cdf_sample = np.loadtxt("cumulative_frequency_sample.dat").T
+
 ########################################################################
-# Interpolate the pairing function: PDF and CDF
+# Plot PDF
 ########################################################################
+# fig, ax = plt.subplots()
+# ax.pcolormesh(
+#     primary_mass_sample, mass_ratio_sample,
+#     pdf_sample, cmap=cmap, rasterized=True
+# )
+# ax.contour(
+#     primary_mass_sample, mass_ratio_sample,
+#     np.log10(pdf_sample), colors="k", levels=10
+# )
+# ax.hlines(0.1, m_min, m_max, color="k", ls="dashed")
+# ax.hlines(0.3, m_min, m_max, color="k", ls="dashed")
+# ax.hlines(0.95, m_min, m_max, color="k", ls="dashed")
+# ax.vlines(primary_mass_boundary, 0., 1., ls="dashed")
+# ax.set_xlim(m_min, m_max)
+# ax.set_ylim(0., 1.)
+# ax.set_xscale("log")
+# ax.set_xlabel(r"$m_{1}$")
+# ax.set_ylabel(r"$q$")
+# fig.savefig("./Figures/f_QgivenM1_moe2017_pairing_2d.pdf", dpi=300)
+# fig.savefig("./Figures/f_QgivenM1_moe2017_pairing_2d.jpg", dpi=300)
+# fig.show()
+
+# fig, ax = plt.subplots()
+# ax.pcolormesh(
+#     primary_mass_sample, mass_ratio_sample,
+#     np.log10(cdf_sample),
+#     cmap=cmap, rasterized=True
+# )
+# ax.contour(
+#     primary_mass_sample, mass_ratio_sample,
+#     np.log10(cdf_sample), colors="k",
+#     levels=10
+# )
+# ax.hlines(0.1, m_min, m_max, color="k", ls="dashed")
+# ax.hlines(0.3, m_min, m_max, color="k", ls="dashed")
+# ax.hlines(0.95, m_min, m_max, color="k", ls="dashed")
+# ax.vlines(primary_mass_boundary, 0., 1., ls="dashed")
+# ax.set_xlim(m_min, m_max)
+# ax.set_ylim(0., 1.)
+# ax.set_xscale("log")
+# ax.set_xlabel(r"$m_{1}$")
+# ax.set_ylabel(r"$q$")
+# fig.savefig("./Figures/F_QgivenM1_moe2017_pairing_2d.pdf", dpi=300)
+# fig.savefig("./Figures/F_QgivenM1_moe2017_pairing_2d.jpg", dpi=300)
+# plt.show()
+
+########################################################################
+# Plot CDF
+########################################################################
+# fig, ax = plt.subplots()
+# for m, f_m in zip(primary_mass_sample[::50], pdf_sample.T[::50]):
+#     ax.plot(mass_ratio_sample, f_m,
+#             label=r"$m_{{1}} = {:1.3f}~\mathrm{{M}}_{{\odot}}$".format(m))
+# ax.set_xlim(0., 1.)
+# ax.set_ylim(0., 5.)
+# ax.set_xlabel(r"$q$")
+# ax.set_ylabel(r"$f_{Q}(\cdot|m_{1})$")
+# ax.legend(frameon=False)
+# fig.savefig("./Figures/f_QgivenM1_moe2017_pairing.pdf", dpi=300)
+# fig.savefig("./Figures/F_QgivenM1_moe2017_pairing.jpg", dpi=300)
+# plt.show()
+
+# fig, ax = plt.subplots()
+# for m, F_m in zip(primary_mass_sample[::50], cdf_sample.T[::50]):
+#     ax.plot(mass_ratio_sample, F_m, ls="solid",
+#             label=r"$m_{{1}} = {:1.3f}~\mathrm{{M}}_{{\odot}}$".format(m))
+# ax.set_xlim(0., 1.)
+# ax.set_ylim(0., 1.)
+# ax.set_xlabel(r"$q$")
+# ax.set_ylabel(r"$f_{Q}(\cdot|m_{1})$")
+# ax.legend(frameon=False)
+# fig.savefig("./Figures/F_QgivenM1_moe2017_pairing.pdf", dpi=300)
+# fig.savefig("./Figures/F_QgivenM1_moe2017_pairing.jpg", dpi=300)
+# plt.show()
+
+########################################################################
+# Form the pairing function
+########################################################################
+def f_secondary(m_2, m_1=1.):
+    q = m_2/m_1
+    res = pdf_interp((q, m_1))/m_1
+
+    return res
+    
 pdf_interp = RegularGridInterpolator(
     (mass_ratio_sample, primary_mass_sample),
-    pdf_sample.T,
+    pdf_sample,
     bounds_error=False,
     fill_value=0.
 )
 cdf_interp = RegularGridInterpolator(
     (mass_ratio_sample, primary_mass_sample),
-    cdf_sample.T,
+    cdf_sample,
     bounds_error=False,
     fill_value=0.
 )
 
-m = np.logspace(np.log10(m_min), np.log10(60), 2**9)
-m1m1, m2m2 = np.meshgrid(m, m)
-f = pdf_interp((m2m2/m1m1, m1m1))
-F = cdf_interp((m2m2/m1m1, m1m1))
-
+########################################################################
+# Plot the pairing function
+########################################################################
+n = 1_000
+m_1 = np.hstack([
+    np.linspace(m_min, 1.2, n),
+    np.linspace(1.2, 3.5, n)[1:],
+    np.linspace(3.5, 6., n)[1:],
+    np.linspace(6., m_max, n)[1:],
+])
+m1m1, m2m2 = np.meshgrid(m_1, m_1)
+f_M2 = f_secondary(m2m2, m1m1)
 fig, ax = plt.subplots()
-ax.pcolormesh(m, m, np.log10(f), cmap="Grays", rasterized=True)
-ax.contour(m, m, f, colors="k", levels=10)
-ax.plot(m, 0.1*m, color="k", ls="solid")
-ax.plot(m, 0.3*m, color="k", ls="dotted")
-ax.plot(m, m, color="k", ls="solid")
-ax.vlines(primary_mass_boundary, m_min, m_max, ls="dotted")
-ax.set_xlim(m_min, m_max)
-ax.set_ylim(m_min, m_max)
+for i, f_i in zip(m_1[::1_000], f_M2[::1_000]):
+    ax.plot(
+        m_1,
+        f_i/np.max(f_i),
+        ls="solid",
+        label=r"$m_{{1}} = {:1.3f}~\mathrm{{M}}_{{\odot}}$".format(i)
+    )
+    I = quad(f_secondary, m_min, m_max)
+    print(I)
+# ax.set_ylim(0., 5.)
 ax.set_xscale("log")
-ax.set_yscale("log")
-ax.set_xlabel(r"$m_{1}$")
-ax.set_ylabel(r"$m_{2}$")
-fig.savefig("./Figures/f_M2givenM2_moe2017_pairing.pdf", dpi=300)
-fig.savefig("./Figures/f_M2givenM2_moe2017_pairing.jpg", dpi=300)
+ax.set_xlabel(r"$q$")
+ax.set_ylabel(r"$f_{Q}(\cdot|m_{1})$")
+ax.legend(frameon=False)
+fig.savefig("./Figures/F_QgivenM1_moe2017_pairing.pdf", dpi=300)
+fig.savefig("./Figures/F_QgivenM1_moe2017_pairing.jpg", dpi=300)
 plt.show()
 
-fig, ax = plt.subplots()
-ax.pcolormesh(m, m, np.log10(F), cmap="Grays", rasterized=True)
-ax.contour(m, m, np.log10(F), colors="k", levels=10)
-ax.plot(m, 0.1*m, color="k", ls="solid")
-ax.plot(m, 0.3*m, color="k", ls="dotted")
-ax.plot(m, m, color="k", ls="solid")
-ax.vlines(primary_mass_boundary, m_min, m_max, ls="dotted")
-ax.set_xlim(m_min, m_max)
-ax.set_ylim(m_min, m_max)
-ax.set_xscale("log")
-ax.set_yscale("log")
-ax.set_xlabel(r"$m_{1}$")
-ax.set_ylabel(r"$m_{2}$")
-fig.savefig("./Figures/F_M2givenM2_moe2017_pairing.pdf", dpi=300)
-fig.savefig("./Figures/F_M2givenM2_moe2017_pairing.jpg", dpi=300)
-plt.show()
