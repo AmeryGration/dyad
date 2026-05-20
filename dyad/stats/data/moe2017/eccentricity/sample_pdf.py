Fix normalization of  random variables

dyad/stats/data/moe2017/eccentricity/sample_pdf.py
@@ -52,7 +52,7 @@ for i, key_i in enumerate(key_log10_primary_mass):
             )
 
 # Normalize histogram of eccentricity
-cumsum = np.cumsum(counts*0.1, axis=2)
+cumsum = np.cumsum(counts*eccentricity_width, axis=2)
 counts /= cumsum[:,:,-1][:,:,None]
 cumsum /= cumsum[:,:,-1][:,:,None]
 
@@ -80,11 +80,11 @@ mpl.style.use("sm")
 fig, ax = plot.plot()
 ax.stairs(counts[0][2], edges_eccentricity, color="red")
 ax.stairs(counts[-1][2], edges_eccentricity, color="magenta")
-plt.savefig("./Figures/eccentricity_0_hist.pdf")
+# plt.savefig("./Figures/eccentricity_0_hist.pdf")
 plt.show()
 
 fig, ax = plot.plot()
 ax.stairs(counts[0][-1], edges_eccentricity, color="red")
 ax.stairs(counts[-1][-1], edges_eccentricity, color="magenta")
-plt.savefig("./Figures/eccentricity_1_hist.pdf")
+# plt.savefig("./Figures/eccentricity_1_hist.pdf")
 plt.show()

dyad/stats/data/moe2017/log_period/sample_pdf.py
@@ -9,7 +9,7 @@ with open("../dykes_data.json") as f:
 
 log10_period = moe2017["log10M1"][0]["0.0"]["logP"].keys()
 log10_period = np.array([float(key) for key in log10_period])
-log10_period_width = np.diff(log10_period)[0]
+log10_period_width = 0.5
 edges_log10_period = log10_period - log10_period_width/2.
 edges_log10_period = np.append(
     edges_log10_period, edges_log10_period[-1] + log10_period_width
@@ -17,7 +17,7 @@ edges_log10_period = np.append(
 
 log10_primary_mass = moe2017["log10M1"][0].keys()
 log10_primary_mass = np.array([float(key) for key in log10_primary_mass])
-log10_primary_mass_width = np.diff(log10_primary_mass)[0]
+log10_primary_mass_width = 0.1
 edges_log10_primary_mass = log10_primary_mass - log10_primary_mass_width/2.
 edges_log10_primary_mass = np.append(
     edges_log10_primary_mass,
@@ -29,7 +29,7 @@ counts = np.array([
      for j in moe2017["log10M1"][0]["0.0"]["logP"].keys()]
      for i in moe2017["log10M1"][0].keys()
 ])
-cumsum = np.cumsum(counts*0.1, axis=1)
+cumsum = np.cumsum(counts*log10_period_width, axis=1)
 counts /= cumsum[:,-1][:,None]
 cumsum /= cumsum[:,-1][:,None]
 
@@ -40,7 +40,7 @@ data = {
     "cumsum": cumsum.tolist(),
 }
 
-with open("moe2017.json", "w") as f:
+with open("data.json", "w") as f:
     json.dump(data, f, indent=2)
 
 # ########################################################################

dyad/stats/data/moe2017/mass_ratio/sample_pdf.py
@@ -51,7 +51,7 @@ for i, key_i in enumerate(key_log10_primary_mass):
             )
 
 # Normalize histogram of mass_ratio
-cumsum = np.cumsum(counts*0.1, axis=2)
+cumsum = np.cumsum(counts*mass_ratio_width, axis=2)
 counts /= cumsum[:,:,-1][:,:,None]
 cumsum /= cumsum[:,:,-1][:,:,None]
 
@@ -66,24 +66,24 @@ data = {
 with open("moe2017.json", "w") as f:
     json.dump(data, f, indent=2)
 
-# ########################################################################
-# # Plot PDFs and CDFs
-# ########################################################################
+########################################################################
+# Plot PDFs and CDFs
+########################################################################
 
-# import plot
-# import matplotlib as mpl
-# import matplotlib.pyplot as plt
+import plot
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-# mpl.style.use("sm")
+mpl.style.use("sm")
 
-# fig, ax = plot.plot()
-# ax.stairs(counts[0][2], edges_mass_ratio, color="red")
-# ax.stairs(counts[-1][2], edges_mass_ratio, color="magenta")
-# plt.savefig("./Figures/mass_ratio_0_hist.pdf")
-# # plt.show()
+fig, ax = plot.plot()
+ax.stairs(counts[0][2], edges_mass_ratio, color="red")
+ax.stairs(counts[-1][2], edges_mass_ratio, color="magenta")
+plt.savefig("./Figures/mass_ratio_0_hist.pdf")
+plt.show()
 
-# fig, ax = plot.plot()
-# ax.stairs(counts[0][-1], edges_mass_ratio, color="red")
-# ax.stairs(counts[-1][-1], edges_mass_ratio, color="magenta")
-# plt.savefig("./Figures/mass_ratio_1_hist.pdf")
-# plt.show()
+fig, ax = plot.plot()
+ax.stairs(counts[0][-1], edges_mass_ratio, color="red")
+ax.stairs(counts[-1][-1], edges_mass_ratio, color="magenta")
+plt.savefig("./Figures/mass_ratio_1_hist.pdf")
+plt.show()

dyad/stats/log_period.py
@@ -281,10 +281,10 @@ class moe2017_hist_gen(sp.stats.rv_continuous):
 
     def _pdf(self, x, log10_primary_mass):
         idx_x = np.searchsorted(
-            self._xedges, x, side="right"
+            self._xedges, x #, side="right"
         )
         idx_primary_mass = np.searchsorted(
-            self._yedges, log10_primary_mass, side="right"
+            self._yedges, log10_primary_mass #, side="right"
         )
         res = self._counts[idx_primary_mass, idx_x]
 
@@ -293,7 +293,7 @@ class moe2017_hist_gen(sp.stats.rv_continuous):
     def _cdf(self, x, log10_primary_mass):
         def _fun(x, log10_primary_mass):
             idx_primary_mass = np.searchsorted(
-                self._yedges, log10_primary_mass, side="right"
+                self._yedges, log10_primary_mass #, side="right"
             )
             res = np.interp(x, self._xedges, self._cumsum[idx_primary_mass])
 
@@ -309,7 +309,9 @@ with open(files(path).joinpath("data.json"), "r") as f:
     _moe2017_hist_data = json.load(f)
 
 edges_log10_period = np.array(_moe2017_hist_data["edges_log10_period"])
-edges_log10_primary_mass = np.array(_moe2017_hist_data["edges_log10_primary_mass"])
+edges_log10_primary_mass = np.array(
+    _moe2017_hist_data["edges_log10_primary_mass"]
+)
 counts = np.array(_moe2017_hist_data["counts"])
 cumsum = np.array(_moe2017_hist_data["cumsum"])
 